107ffadbfea4cd573590b24b3901763c870ebde4
Nicole Andrews <nicoleandrews@MacBookPro.home>
2025-08-23 18:35:44 -0600

    implemented thread-safe aggregation of results using a mutex

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
 #include "LogParser.h"
+
+std::map<std::string, int> final_report_counts;
+int final_total_lines = 0;
+std::mutex report_mutex;
+
+void parse_file_worker(const std::string& filename) {
+    LogParser parser(filename);
+
+    if (parser.parse()) {
+        std::lock_guard<std::mutex> lock(report_mutex);
+
+        final_total_lines += parser.getTotalLines();
+        final_report_counts["INFO"] += parser.getInfoCount();
+        final_report_counts["WARNING"] += parser.getWarningCount();
+        final_report_counts["ERROR"] += parser.getErrorCount();
+    } else {
+        std::cout << "Thread for '" << filename << "' failed to parse file." << std::endl;
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -11,22 +31,25 @@ int main(int argc, char* argv[]) {
     
     std::vector<std::thread> threads;
 
+    for (int i = 1; i < argc; ++i) {
+        std::string filename = argv[i];
+        threads.emplace_back(parse_file_worker, filename);
+    }
+
+    std::cout << "Launched " << threads.size() << " worker threads. Waiting for them to finish..." << std::endl;
+
     LogParser parser(argv[1]);
 
-    if (parser.parse()) {
-        std::cout << "Log File Analysis Complete" << std::endl;
-        std::cout << "-------------------------" << std::endl;
-        std::cout << "Total Lines Processed: " << parser.getTotalLines() << std::endl;
-        std::cout << "INFO messages: " << parser.getInfoCount() << std::endl;
-        std::cout << "WARNING messages: " << parser.getWarningCount() << std::endl;
-        std::cout << "ERROR messages: " << parser.getErrorCount() << std::endl;
+    for (auto& t : threads) {
+        t.join();  
     }
 
-    for (auto& t : threads) {
-        if (t.joinable()) {
-            t.join();
-        }
-    }
+    std::cout << "All threads finished." << std::endl;
+    std::cout << "--- Final Report ---" << std::endl;
+    std::cout << "Total Lines Processed: " << final_total_lines << std::endl;
+    std::cout << "INFO messages: " << final_report_counts["INFO"] << std::endl;
+    std::cout << "WARNING messages: " << final_report_counts["WARNING"] << std::endl;
+    std::cout << "ERROR messages: " << final_report_counts["ERROR"] << std::endl;
     
     return 0;
 }
